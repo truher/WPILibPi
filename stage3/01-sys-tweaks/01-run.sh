0fb57108c41b5d60ef9c1b1bace9a92681c6d1dd
Peter Johnson <johnson.peter@gmail.com>
2022-03-13 23:28:08 -0700

    Update to WPILib 2022.4.1 (#221)

--- stage3/01-sys-tweaks/01-run.sh
+++ stage3/01-sys-tweaks/01-run.sh
@@ -50,7 +50,7 @@ wget -nc -nv \
 
 # allwpilib
 wget -nc -nv -O allwpilib.tar.gz \
-    https://github.com/wpilibsuite/allwpilib/archive/v2022.3.1.tar.gz
+    https://github.com/wpilibsuite/allwpilib/archive/v2022.4.1.tar.gz
 
 # pynetworktables
 wget -nc -nv -O pynetworktables.tar.gz \
