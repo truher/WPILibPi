421640766bfa3af7067da5cc5b1b1b609840254a
Peter Johnson <johnson.peter@gmail.com>
2023-01-02 17:27:02 -0800

    Disable edgetpu and upgrade Python

--- stage2/00-configure-apt/00-run.sh
+++ stage2/00-configure-apt/00-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-echo "adding repo for edgetpu"
-echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" > "${ROOTFS_DIR}/etc/apt/sources.list.d/coral-edgetpu.list"
+#echo "adding repo for edgetpu"
+#echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" > "${ROOTFS_DIR}/etc/apt/sources.list.d/coral-edgetpu.list"
 
-on_chroot apt-key add - < files/edgetpu.gpg
-on_chroot << EOF
-apt-get update
-apt-get dist-upgrade -y
-EOF
+#cp files/edgetpu.gpg "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/coral-edgetpu.gpg"
+#on_chroot << EOF
+#apt-get update
+#apt-get dist-upgrade -y
+#EOF
