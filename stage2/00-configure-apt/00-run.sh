41431b22fc8987ee85a2fbf946944c6133010b9f
Peter Johnson <johnson.peter@gmail.com>
2023-02-05 17:30:43 -0800

    Use local gpg to install edgetpu certificate

--- stage2/00-configure-apt/00-run.sh
+++ stage2/00-configure-apt/00-run.sh
@@ -3,7 +3,7 @@
 echo "adding repo for edgetpu"
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" > "${ROOTFS_DIR}/etc/apt/sources.list.d/coral-edgetpu.list"
 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | on_chroot apt-key add -
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor > "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/google.gpg"
 on_chroot << EOF
 apt-get update
 apt-get dist-upgrade -y
